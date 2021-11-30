4bcf3b54323710ce1ddc59aae4f58643ed492c44
Yasin Beyazlı <yasinbeyazli29@gmail.com>
2021-11-30 15:57:35 +0300

    Server is started

---
+++ v1/src/app.js
@@ -0,0 +1,16 @@
+const express = require("express");
+const helmet = require("helmet");
+const config = require("./config");
+const loaders = require("./loaders");
+
+config();
+loaders();
+
+const app = express();
+
+app.use(express.json());
+app.use(helmet());
+
+app.listen(process.env.APP_PORT, () => {
+  console.log("Sunucu ayağa kalktı 🚀🚀");
+});
